68486ff61d83183abd16deb20e25aaceaa6e3ca7
trungtin1811 <trungtin.maicogroup@gmail.com>
2022-04-14 16:05:16 +0700

    fix

---
+++ ShopManagement/trungtin.products/fans/StandFans.cs
@@ -0,0 +1,28 @@
+﻿using ShopManagement.trungtin.fans;
+using ShopManagement.trungtin.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.trungtin.products.fans
+{
+    internal class StandFans : Fans
+    {
+        private const string ID_REGEX = "^SDF[0-9]{3}$";
+
+        public StandFans() { input(); }
+        public StandFans(string id, string name, string placeOfProduction) : base(id, name, placeOfProduction)
+        {
+            this._price = 500;
+        }
+        public void input()
+        {
+            this._id = MyIO.getRegexString(ID_REGEX, "\t\t + Input Stand Fan ID (SDFXXX - X stands for a number): ", "\t\t ID is invalid! Please try again!");
+            this._name = "Stand Fan";
+            this._placeOfProduction = MyIO.getString("\t\t + Input Place of Production: ", "\t\t Place of Production cannot be empty! Please try again!");
+            this._price = 500;
+        }
+    }
+}
---
+++ ShopManagement/trungtin.products/fans/SteamFans.cs
@@ -0,0 +1,36 @@
+﻿using ShopManagement.trungtin.fans;
+using ShopManagement.trungtin.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.trungtin.products.fans
+{
+    internal class SteamFans : Fans
+    {
+        private double _waterCapacity;
+        private const string ID_REGEX = "^SMF[0-9]{3}$";
+
+        public SteamFans() { input(); }
+        public SteamFans(string id, string name, string placeOfProduction, double waterCapacity) : base(id, name, placeOfProduction)
+        {
+            WaterCapacity = waterCapacity;
+            this._price = waterCapacity * 400;
+        }
+        public double WaterCapacity
+        {
+            get => _waterCapacity;
+            set => _waterCapacity = value;
+        }
+        public void input()
+        {
+            this._id = MyIO.getRegexString(ID_REGEX, "\t\t + Input Steam Fan ID (SMFXXX - X stands for a number): ", "\t\t ID is invalid! Please try again!");
+            this._name = "Steam Fan";
+            this._placeOfProduction = MyIO.getString("\t\t + Input Place of Production: ", "\t\t Place of Production cannot be empty! Please try again!");
+            this._waterCapacity = MyIO.getDouble(0, "\t\t + Input Water Capacity: ");
+            this._price = _waterCapacity * 400;
+        }
+    }
+}
